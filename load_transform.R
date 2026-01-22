5f3e4456473ba7e15c7139d051fd855a880d409e
Keshav-Lenovo <keshavbhatt@outlook.com>
2026-01-22 14:03:53 +0100

    calculated deltas

--- load_transform.R
+++ load_transform.R
@@ -2,52 +2,56 @@ library(tidyverse)
 library(lubridate)
 library(vroom)
 
-saudi_cds_10y_raw <- read.csv("./data/SAGV10YUSAC.csv")
+# Functions
+# TODO: Move outside
+calc_price_delta <- function(data, date_col, price_col) {
+  # 1. Sort the data frame by the date column
+  # We use data[[date_col]] to extract the column as a vector
+  data <- data[order(data[[date_col]]), ]
+    
+  # 2. Calculate the difference (Current - Previous)
+  # diff() calculates the change; we pad with NA to maintain   row length
+  delta_values <- c(NA, diff(data[[price_col]]))
+    
+  # 3. Assign the result to a new column
+  data[["d_cds"]] <- delta_values
+    
+  return(data)
+}
 
-saudi_cds_10y <- saudi_cds_10y_raw[, c("Date", "Change")]
+# Usage:
+# Note: Column names must be passed as strings in quotes
+saudi_cds_10y <- calc_cds_delta(saudi_cds_10y, "Date", "Price")
+
+
+# Usage:
+saudi_cds_10y <- calc_cds_delta(saudi_cds_10y)
+
+# Read Raw Data
+saudi_cds_10y_raw <- read.csv("./data/SAGV10YUSAC.csv")
+us_10y_raw <- read.csv("./data/United-States-10-YearBondYieldHistoricalData.csv", 
+                       header=TRUE)
+
+
+## CDS
+saudi_cds_10y <- saudi_cds_10y_raw[, c("Date", "Price")]
 
 saudi_cds_10y$Date <- dmy(saudi_cds_10y$Date)
 
-saudi_cds_10y$Change <- as.numeric(gsub("%", "", saudi_cds_10y$Change))
+saudi_cds_10y <- calc_price_delta(data = saudi_cds_10y, date_col = "Date", price_col = "Price")
 
 
-## Day when swap agreement with China was announced
-event <- ymd("2023-11-20")
 
-## Create estimation data set (-1 year, 30 days)
-cds_est_data <- saudi_cds_10y %>%
-  filter(Date >= ymd('2022-11-20') &
-           Date <= (event - 30))
-
-## And observation data
-### Core: [-1,1], we have to do -3 days because the announcement was on a Monday
-obs_data_1 <- saudi_cds_10y %>%
-  filter(Date >= event - days(3) & 
-           Date <= event + days(1)) 
-
-### Leak: [-3, 3]
-obs_data_3 <- saudi_cds_10y %>%
-  filter(Date >= event - days(5) & 
-           Date <= event + days(3)) 
-
-### Sanity Check: [-5, 5]
-obs_data_5 <- saudi_cds_10y %>%
-  filter(Date >= event - days(8) & 
-           Date <= event + days(7)) 
-
-
-# US 10 year yields
-us_10y_raw <- read.csv("./data/United-States-10-YearBondYieldHistoricalData.csv", 
-                       header=TRUE, 
-                       colClasses = "character")
-
-
-us_10y_raw$Change <- as.numeric(gsub("%", "", us_10y_raw$Change))
-                                
+## US 10 year yields
 
 us_10y_raw$Date <- mdy(us_10y_raw$Date) 
+us_10y <- us_10y_raw[, c("Date", "Price")]
 
-## Create estimation data set (-1 year, 30 days)
-us_10y_est_data <- us_10y_raw %>%
-  filter(Date >= ymd('2022-11-20') &
-           Date <= (event - 30))
+
+us_10y <- calc_price_delta(data = us_10y, date_col = "Date", price_col = "Price")
+
+
+
+
+
+
